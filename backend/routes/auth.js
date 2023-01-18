3474bd3e8c0885f8b4f7f455c0d7297fc318d808
Akash Pandey <akashp519519@gmail.com>
2023-01-18 23:48:52 +0530

    Salt and authtoken

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,36 +1,60 @@
-const express = require('express');
-const User = require('../models/User');
+const express = require("express");
+const User = require("../models/User");
 const router = express.Router();
-const { body, validationResult } = require('express-validator');
+const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-
+const JWT_SECRET = "AKASGUDCGFGX";
 
 // Create a User using: POST "/api/auth/createuser". No login required.
-router.post('/createuser', [body('email', 'Enter a Valid Email').isEmail(), body('name', 'Enter a Valid Name').isLength({ min: 3 }), body('password', 'Password must be atleast 5 characters').isLength({ min: 5 })], async (req, res) => {
-  // If there are errors, return Bad request and the errors.
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-  // Check whether the user with the same email exists already.
-  try {
-    let user = await User.findOne({ email: req.body.email });
-    if (user) {
-      return res.status(400).json({ error: "Sorry a user with this email already exists." })
+router.post(
+  "/createuser",
+  [
+    body("email", "Enter a Valid Email").isEmail(),
+    body("name", "Enter a Valid Name").isLength({ min: 3 }),
+    body("password", "Password must be atleast 5 characters").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    // If there are errors, return Bad request and the errors.
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
+    try {
+      // Check whether the user with the same email exists already.
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry a user with this email already exists." });
+      }
 
-    user = await User.create({
-      name: req.body.name,
-      password: req.body.password,
-      email: req.body.email
-    })
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
+      // Create a new user
+      user = await User.create({
+        name: req.body.name,
+        password: secPass,
+        email: req.body.email,
+      });
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
 
-    res.json(user)
+      const authtoken = jwt.sign(data, JWT_SECRET);
+
+      // res.json(user)
+      res.json({ authtoken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Some Error occured");
+    }
   }
-  catch (error) {
-    console.error(error.message)
-    res.status(500).send("Some Error occured");
-  }
-})
+);
 
-module.exports = router
+module.exports = router;
