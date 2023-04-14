647eb4da239ca1a209de102f06cdb6f5df8997b1
Akash Pandey <akashp519519@gmail.com>
2023-04-14 13:17:11 +0530

    Mongodb Connected and Node Server Running.

---
+++ backend/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://127.0.0.1:27017" // changed localhost:27017 to 127.0.0.1:27017 because mongoose 
+// was resolving localhost as wrong value.  
+
+const connectToMongo = ()=>{
+    mongoose.connect(mongoURI).then( console.log("Connected to Mongo Succesfully"))
+}
+
+module.exports = connectToMongo;
